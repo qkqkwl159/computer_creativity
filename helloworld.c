655a1b16b2e8db7af30d3821ba7795a827510212
qkqkwl159 <80611841+qkqkwl159@users.noreply.github.com>
2023-04-12 18:00:03 +0900

    main commit1

---
+++ helloworld.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+void issue1(){
+
+
+	printf("issue 1 \n");
+
+	return;
+}
+
+void issue2(){
+
+
+	printf("issue 2 \n");
+
+	return;
+}
+
+
+int main(void){
+
+
+	issue1();
+	issue2();
+
+	return 0;
+
+
+}
