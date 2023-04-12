26605103e55347608ee1e8f2d2512369c2997425
qkqkwl159 <80611841+qkqkwl159@users.noreply.github.com>
2023-04-12 18:46:47 +0900

    main file update

--- helloworld.c
+++ helloworld.c
@@ -1,33 +1,10 @@
 #include <stdio.h>
 
 
-void issue1(){
-
-
-	printf("issue 1 \n");
-	printf("issue 1 APP add\n");
-
-	return;
-}
-
-void issue2(){
-
-
-	printf("issue 2 \n");
-	printf("issue 2 APP add\n");
-	printf("confilct!\n");
-
-	return;
-}
-
-
 int main(void){
 
 
-	issue1();
-	issue2();
-	printf("i2\n");
-
+	printf("Hello");
 	return 0;
 
 
